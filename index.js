d8808acf22efe17d3189ea78058f51128e33965d
Jooyon Won <jyon1204@gmail.com>
2019-04-13 20:20:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function dwarfRollCall(dwarves) {
+  var nameorder = [];
+  for(let i = 0; i < dwarves.length; i++){
+    nameorder += `${i+1}. ${dwarves[i]} `
+  }
+  return nameorder;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var uppercaseCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    var exclamation = "!";
+    uppercaseCalls.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase());
+  }
+return uppercaseCalls;
 }
 
 function longPlaneteerCalls(words) {
+    if (words.length > 4){
+    return true
+  }return false
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++){
+    if (foods[i] === `cheddar`){
+    return foods[i];
+  }
+}return "no cheese!"
 }
