657f14450fcfc4132637938aaead3c286c4943b6
Jooyon Won <jyon1204@gmail.com>
2019-04-15 14:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-function dwarfRollCall(dwarves) {
-  var nameorder = [];
-  for(let i = 0; i < dwarves.length; i++){
-    nameorder += `${i+1}. ${dwarves[i]} `
+
+function dwarfRollCall(dwarves){
+    var nameorder = [];
+  for (let i = 0; i < dwarves.length; i++){
+   nameorder += `${i+1}. ${dwarves[i]} `
   }
   return nameorder;
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
-  var uppercaseCalls = [];
-  for(let i = 0; i < planeteerCalls.length; i++){
-    var exclamation = "!";
-    uppercaseCalls.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase());
+  var upperCaseCalls = [];
+  var exclamation = "!";
+  for (let i = 0; i < planeteerCalls.length; i++){
+    upperCaseCalls.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase());
   }
-return uppercaseCalls;
+return upperCaseCalls;
 }
 
-function longPlaneteerCalls(words) {
-    if (words.length > 4){
-    return true
+function longPlaneteerCalls(words){
+  for (let i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+     return true
+    }
   }return false
 }
 
-function findTheCheese (foods) {
-  for (let i = 0; i < foods.length; i++){
-    if (foods[i] === `cheddar`){
-    return foods[i];
-  }
-}return "no cheese!"
+
+function findTheCheese(grocery){
+  for (let i = 0; i < grocery.length; i++){
+    if (grocery[i] === `cheddar`||grocery[i]===`gouda`||grocery[i]===`camembert`){
+    return grocery[i];
+    }
+  }return "no cheese!";
 }
